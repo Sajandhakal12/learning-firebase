1c8c89fd2a88ef715bed9931ca30f483f1e727d0
codedestroyer12 <sajan12dhakal@gmail.com>
2020-05-07 18:03:19 +0545

    First commit

---
+++ social media app/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDc2qw9U3aH6WFSUNFZUHNKspcX3qJqSdM",
+  authDomain: "socialape-c9a67.firebaseapp.com",
+  databaseURL: "https://socialape-c9a67.firebaseio.com",
+  projectId: "socialape-c9a67",
+  storageBucket: "socialape-c9a67.appspot.com",
+  messagingSenderId: "687808088207",
+  appId: "1:687808088207:web:ab19e3fbcdd0f632a508fa",
+  measurementId: "G-3NZFPPJVJE",
+};
